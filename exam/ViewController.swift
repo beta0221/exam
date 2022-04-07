fa626112b8529ad2565807638f6a1560ebd32ef8
beta0221 <beta0221@gmail.com>
2022-04-07 18:51:25 +0800

    Initial Commit

---
+++ exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exam
+//
+//  Created by 林奕儒 on 2022/4/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
